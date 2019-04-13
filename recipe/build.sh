647405e400793bef2d630d3cd79745ddb6035796
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-04-12 20:31:40 -0400

    patch for missing more in CI, stricter bash

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+set -eux
 
 # Ensure the timestamp dependencies do not cause us to need
 # to run autoreconf-y stuff (which I tried but it is not in
 # a working state at present with gawk 4.2.1).
+mv "bootstrap.sh?h=${PKG_NAME}-${PKG_VERSION}" bootstrap.sh
 chmod +x ./bootstrap.sh
 ./bootstrap.sh
 
 ./configure --prefix="${PREFIX}" \
             --with-readline="${PREFIX}"
 
-make -j${NUM_CPUS} ${VERBOSE_AT}
+make -j${CPU_COUNT} V=1
 
 # These tests fail under emulation, still run them but ignore their result
 if [[ ${target_platform} == linux-aarch64 ]]; then
-    make check -j${NUM_CPUS} || true
+    make check -j${CPU_COUNT} || true
 elif [[ ${target_platform} == linux-ppc64le ]]; then
-    make check -j${NUM_CPUS} || true
+    make check -j${CPU_COUNT} || true
 else
-    make check -j${NUM_CPUS}
+    make check -j${CPU_COUNT}
 fi
 
 make install
